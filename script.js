65fb4947cfdd56f05113dc161ed9f6d7f3ba4d04
NextinMono <lorenzorai05@gmail.com>
2021-11-16 18:19:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //I'm guessing these are animations
 $(document).ready(() => {
     $(".header h1").css("opacity", "1");
+    $(".header h1").css("color", "#ffffff");
     setTimeout(() => $("h3").css("opacity", "1"), 500);
     setTimeout(() => $(".spettro").css({
         "transform": "translateX(0%)", 
