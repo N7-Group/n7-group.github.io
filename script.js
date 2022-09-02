57ca9b9239be141041ba8159f4aad6fc80d91e4d
NextinMono <lorenzorai05@gmail.com>
2022-09-02 12:37:40 +0200

    Updates to looks and animations

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ $(document).ready(() => {
     $(".header h1").css("opacity", "1");
     $(".header h1").css("color", "#ffffff");
     setTimeout(() => $("h3").css("opacity", "1"), 500);
-    setTimeout(() => $(".image-view").css("opacity", "1"), 500);
+    setTimeout(() => $(".image-view").css("opacity", "1"), 200);
     setTimeout(() => $(".monospace").css({
-        "transform": "translateX(0%)", 
         "opacity": "1"
     }), 250);
     setTimeout(() => $(".text-ms").css({
-        "transform": "translateX(-0%)",
         "opacity": "1"
-    }), 1000);
+    }), 250);
+
 })
