8041e66848524d124322d7b9ea2f86a7e2e436ef
NextinMono <lorenzorai05@gmail.com>
2023-03-25 14:01:55 +0100

    Revert

--- extras.js
+++ extras.js
@@ -37,4 +37,4 @@ $(document).ready(() => {
     }
 });
 
-$(window).scroll(() =>  $("nav").addClass("nav-fixed"))
+$(window).scroll(() => !$(window).scrollTop() ? $("nav").removeClass("nav-fixed") : $("nav").addClass("nav-fixed"))
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //I'm guessing these are animations
 $(document).ready(() => {
-    // $(".header h1").css("opacity", "1");
-    // $(".header h1").css("color", "#ffffff");
+    $(".header h1").css("opacity", "1");
+    $(".header h1").css("color", "#ffffff");
     setTimeout(() => $("h3").css("opacity", "1"), 500);
     setTimeout(() => $(".image-view").css("opacity", "1"), 200);
     setTimeout(() => $(".monospace").css({
