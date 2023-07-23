44cf294477b6ebb4e7375632a867b40849be8bc8
NextinMono <lorenzorai05@gmail.com>
2023-07-23 20:25:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ $(document).ready(() => {
     $(".header h1").css("opacity", "1");
     $(".container").css("opacity", "0");
     $(".container-styled").css("opacity", "0");
+    $(".container-styleless").css("opacity", "0");
     $(".header h1").css("color", "#ffffff");
     setTimeout(() => $("h3").css("opacity", "1"), 500);
     setTimeout(() => $(".image-view").css("opacity", "1"), 200);
     setTimeout(() => $(".monospace").css({
         "opacity": "1"
     }), 250);
+    setTimeout(() => $(".header h1").css({
+        "opacity": "1"
+    }), 250);
     setTimeout(() => $(".container").css({
         "opacity": "1"
     }), 250);
     setTimeout(() => $(".container-styled").css({
+        "opacity": "1"
+    }), 250);
+    setTimeout(() => $(".container-styleless").css({
         "opacity": "1"
     }), 250);
     setTimeout(() => $(".text-ms").css({
