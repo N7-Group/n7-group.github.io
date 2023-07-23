a3cca6e2c4d21130a5428415b4bfd285a703a6d2
Lorenzo Raineri <lorenzorai05@gmail.com>
2023-07-23 18:56:49 +0200

    Scale effect

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 //Animations
 $(document).ready(() => {
     $(".header h1").css("opacity", "1");
+    $(".container").css("opacity", "0");
+    $(".container-styled").css("opacity", "0");
     $(".header h1").css("color", "#ffffff");
     setTimeout(() => $("h3").css("opacity", "1"), 500);
     setTimeout(() => $(".image-view").css("opacity", "1"), 200);
     setTimeout(() => $(".monospace").css({
+        "opacity": "1"
+    }), 250);
+    setTimeout(() => $(".container").css({
+        "opacity": "1"
+    }), 250);
+    setTimeout(() => $(".container-styled").css({
         "opacity": "1"
     }), 250);
     setTimeout(() => $(".text-ms").css({
